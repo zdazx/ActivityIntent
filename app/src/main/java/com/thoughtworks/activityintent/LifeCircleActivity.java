95302b5a67f0770abf7af38ab276fe7c12d89885
zdazx <zhangduo0227@163.com>
2020-07-21 17:58:32 +0800

    add life circle method

--- app/src/main/java/com/thoughtworks/activityintent/LifeCircleActivity.java
+++ app/src/main/java/com/thoughtworks/activityintent/LifeCircleActivity.java
@@ -10,5 +10,38 @@ public class LifeCircleActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_circle);
+        System.out.println("--- Life Circle start ---");
+        System.out.println("--- onCreate ---");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("--- onStart ---");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("--- onResume ---");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("--- onPause ---");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("--- onStop ---");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("--- onDestroy ---");
+        System.out.println("--- Life Circle End ---");
     }
 }
