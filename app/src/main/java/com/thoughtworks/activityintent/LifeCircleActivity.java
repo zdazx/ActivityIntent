8bab8819955f44a36adb7330af9af2d5106570f7
zdazx <zhangduo0227@163.com>
2020-07-21 16:09:04 +0800

    add empty layout and activity to view life circle

---
+++ app/src/main/java/com/thoughtworks/activityintent/LifeCircleActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.activityintent;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LifeCircleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_circle);
+    }
+}
